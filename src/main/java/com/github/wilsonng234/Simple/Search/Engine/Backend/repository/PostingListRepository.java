21c55c42c56001aaab9b4b138c2dd968702a9ffd
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 01:31:37 +0800

    Add findPostingListByWordId

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/repository/PostingListRepository.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/repository/PostingListRepository.java
@@ -5,6 +5,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostingListRepository extends MongoRepository<PostingList, ObjectId> {
+    Optional<PostingList> findPostingListByWordId(String wordId);
 }

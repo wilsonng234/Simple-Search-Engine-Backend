8b7d8565e83a54f4f091e75aec0811e8222fe776
Wilson Ng <wilsonng234@gmail.com>
2023-04-13 11:28:27 +0800

    Initial setup

---
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/SimpleSearchEngineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.wilsonng234.Simple.Search.Engine.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSearchEngineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSearchEngineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wilsonng234/Simple/Search/Engine/Backend/SimpleSearchEngineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wilsonng234.Simple.Search.Engine.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSearchEngineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

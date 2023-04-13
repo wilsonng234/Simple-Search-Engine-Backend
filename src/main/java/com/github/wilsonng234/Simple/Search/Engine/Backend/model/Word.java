cd9825dcf3dc2679b2d2d6c068712d92da5f33b8
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 01:44:25 +0800

    Add NoArgsConstructor

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/model/Word.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/model/Word.java
@@ -2,11 +2,13 @@ package com.github.wilsonng234.Simple.Search.Engine.Backend.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "words")
 public class Word {

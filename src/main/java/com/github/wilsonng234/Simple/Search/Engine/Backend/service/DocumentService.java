e4904960e88d13c9d94af548bd538e6fbb9d181a
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 00:03:20 +0800

    Implement post document

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/service/DocumentService.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/service/DocumentService.java
@@ -3,9 +3,11 @@ package com.github.wilsonng234.Simple.Search.Engine.Backend.service;
 import com.github.wilsonng234.Simple.Search.Engine.Backend.model.Document;
 import com.github.wilsonng234.Simple.Search.Engine.Backend.repository.DocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class DocumentService {
@@ -15,4 +17,20 @@ public class DocumentService {
     public List<Document> allDocuments() {
         return documentRepository.findAll();
     }
+
+    public Document createDocument(Document document) {
+        documentRepository.insert(document);
+
+        return document;
+    }
+
+    public Document createDocument(String url, long size, String title, long lastModificationDate,
+                                   Map<String, Integer> titleWordIDFreqPairs,
+                                   Map<String, Integer> bodyWordIDFreqPairs,
+                                   List<String> childrenUrls) {
+        Document createdDocument = new Document(url, size, title, lastModificationDate, titleWordIDFreqPairs, bodyWordIDFreqPairs, childrenUrls);
+
+        return createDocument(createdDocument);
+    }
+
 }

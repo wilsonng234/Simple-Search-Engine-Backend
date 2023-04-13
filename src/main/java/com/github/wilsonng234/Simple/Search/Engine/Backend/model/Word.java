a2c415ca467d686cd9d70dc74da02d9e4a38e523
Wilson Ng <wilsonng234@gmail.com>
2023-04-13 15:30:14 +0800

    Create Word collection

---
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/model/Word.java
@@ -0,0 +1,24 @@
+package com.github.wilsonng234.Simple.Search.Engine.Backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "words")
+public class Word {
+    @Id
+    private ObjectId wordId;
+    @Indexed(unique = true)
+    private String word;
+
+    public Word(String word) {
+        this.word = word;
+    }
+}

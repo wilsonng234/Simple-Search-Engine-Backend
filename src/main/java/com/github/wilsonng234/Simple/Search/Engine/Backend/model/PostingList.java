bb73872f4be95cbe9fc0dde2863b56ffe8c23f3c
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 19:23:48 +0800

    Update PostingList as abstract class

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/model/PostingList.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/model/PostingList.java
@@ -3,17 +3,14 @@ package com.github.wilsonng234.Simple.Search.Engine.Backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-@Document(collection = "postingLists")
-public class PostingList {
+public abstract class PostingList {
     @Id
     private String wordId;
     private List<Posting> postings;
-
 
 }

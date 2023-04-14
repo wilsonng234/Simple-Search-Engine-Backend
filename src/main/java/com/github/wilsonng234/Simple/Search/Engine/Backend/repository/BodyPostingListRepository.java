8cb971fea673f134db7936d8e8c7cecaab40a150
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 20:35:05 +0800

    Add Repository annotation

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/repository/BodyPostingListRepository.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/repository/BodyPostingListRepository.java
@@ -2,9 +2,11 @@ package com.github.wilsonng234.Simple.Search.Engine.Backend.repository;
 
 import com.github.wilsonng234.Simple.Search.Engine.Backend.model.BodyPostingList;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BodyPostingListRepository extends MongoRepository<BodyPostingList, String> {
     Optional<BodyPostingList> findBodyPostingListByWordId(String wordId);
 }

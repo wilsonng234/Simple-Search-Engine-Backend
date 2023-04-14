9e500224c0c3a87e77fb073877dd7b1065a63077
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 19:23:10 +0800

    Update PostingListService as abstract class

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/service/PostingListService.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/service/PostingListService.java
@@ -2,44 +2,15 @@ package com.github.wilsonng234.Simple.Search.Engine.Backend.service;
 
 import com.github.wilsonng234.Simple.Search.Engine.Backend.model.Posting;
 import com.github.wilsonng234.Simple.Search.Engine.Backend.model.PostingList;
-import com.github.wilsonng234.Simple.Search.Engine.Backend.repository.PostingListRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
-public class PostingListService {
-    @Autowired
-    private PostingListRepository postingListRepository;
+public abstract class PostingListService {
+    public abstract List<? extends PostingList> allPostingLists();
 
-    public List<PostingList> allPostingLists() {
-        return postingListRepository.findAll();
-    }
+    public abstract PostingList createPostingList(String wordId);
 
-    public PostingList createPostingList(String wordId) {
-        PostingList postingList = new PostingList(wordId, new LinkedList<>());
-
-        return postingListRepository.insert(postingList);
-    }
-
-    public PostingList putPositingList(String wordId, Posting posting) {
-        Optional<PostingList> postingListOptional = postingListRepository.findPostingListByWordId(wordId);
-        PostingList postingList = postingListOptional.orElseGet(() -> createPostingList(wordId));
-
-        List<Posting> postings = postingList.getPostings();
-        List<Posting> filteredPostings = postings.stream().filter(p -> p.equals(posting)).toList();
-
-        if (filteredPostings.size() == 0) {
-            postings.add(posting);
-        } else {
-            filteredPostings.get(0).setWordPositions(posting.getWordPositions());
-        }
-
-        postingList.setPostings(postings);
-        return postingListRepository.save(postingList);
-    }
+    public abstract PostingList putPositingList(String wordId, Posting posting);
 }

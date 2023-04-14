3e907950895915a79594f4e606df19a9805df777
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 19:28:43 +0800

    Create BodyPostingListService

---
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/service/BodyPostingListService.java
@@ -0,0 +1,45 @@
+package com.github.wilsonng234.Simple.Search.Engine.Backend.service;
+
+import com.github.wilsonng234.Simple.Search.Engine.Backend.model.BodyPostingList;
+import com.github.wilsonng234.Simple.Search.Engine.Backend.model.Posting;
+import com.github.wilsonng234.Simple.Search.Engine.Backend.model.PostingList;
+import com.github.wilsonng234.Simple.Search.Engine.Backend.repository.BodyPostingListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BodyPostingListService extends PostingListService {
+    @Autowired
+    private BodyPostingListRepository bodyPostingListRepository;
+
+    public List<? extends PostingList> allPostingLists() {
+        return bodyPostingListRepository.findAll();
+    }
+
+    public BodyPostingList createPostingList(String wordId) {
+        BodyPostingList bodyPostingList = new BodyPostingList(wordId, new LinkedList<>());
+
+        return bodyPostingListRepository.insert(bodyPostingList);
+    }
+
+    public BodyPostingList putPositingList(String wordId, Posting posting) {
+        Optional<BodyPostingList> postingListOptional = bodyPostingListRepository.findBodyPostingListByWordId(wordId);
+        BodyPostingList bodyPostingList = postingListOptional.orElseGet(() -> createPostingList(wordId));
+
+        List<Posting> postings = bodyPostingList.getPostings();
+        List<Posting> filteredPostings = postings.stream().filter(p -> p.equals(posting)).toList();
+
+        if (filteredPostings.size() == 0) {
+            postings.add(posting);
+        } else {
+            filteredPostings.get(0).setWordPositions(posting.getWordPositions());
+        }
+
+        bodyPostingList.setPostings(postings);
+        return bodyPostingListRepository.save(bodyPostingList);
+    }
+}

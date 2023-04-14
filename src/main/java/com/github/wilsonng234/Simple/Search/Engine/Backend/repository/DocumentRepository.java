456bd50b9c0d91399985e5ca9597ca27d5654227
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 19:23:29 +0800

    Update ObjectId as String

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/repository/DocumentRepository.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/repository/DocumentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentRepository extends MongoRepository<Document, ObjectId> {
+public interface DocumentRepository extends MongoRepository<Document, String> {
 }

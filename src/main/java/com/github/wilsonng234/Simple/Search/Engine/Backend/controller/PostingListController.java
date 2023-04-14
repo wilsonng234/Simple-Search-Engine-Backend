efa2cd535ee85f024b110b4fa07b210d50e1aa99
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 19:28:04 +0800

    Update PostingListController as abstract class

--- src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/controller/PostingListController.java
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/controller/PostingListController.java
@@ -3,37 +3,19 @@ package com.github.wilsonng234.Simple.Search.Engine.Backend.controller;
 import com.github.wilsonng234.Simple.Search.Engine.Backend.model.Posting;
 import com.github.wilsonng234.Simple.Search.Engine.Backend.model.PostingList;
 import com.github.wilsonng234.Simple.Search.Engine.Backend.model.Word;
-import com.github.wilsonng234.Simple.Search.Engine.Backend.service.PostingListService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 
-@RestController
-@CrossOrigin(origins = "*")
-@RequestMapping("api/v1/postingLists")
-public class PostingListController {
-    @Autowired
-    private PostingListService postingListService;
-
+public abstract class PostingListController {
     @GetMapping
-    public ResponseEntity<List<PostingList>> getAllPostingLists() {
-        return new ResponseEntity<>(postingListService.allPostingLists(), HttpStatus.OK);
-    }
+    public abstract ResponseEntity<List<? extends PostingList>> getAllPostingLists();
 
     @PostMapping
-    public ResponseEntity<PostingList> createPostingList(@RequestBody Word word) {
-        String wordId = word.getWordId();
-
-        return new ResponseEntity<>(postingListService.createPostingList(wordId), HttpStatus.CREATED);
-    }
+    public abstract ResponseEntity<? extends PostingList> createPostingList(@RequestBody Word word);
 
     @PutMapping("/{wordId}")
-    public ResponseEntity<PostingList> putPostingList(@PathVariable String wordId, @RequestBody Posting posting) {
-        return new ResponseEntity<>(postingListService.putPositingList(wordId, posting), HttpStatus.CREATED);
-    }
+    public abstract ResponseEntity<? extends PostingList> putPostingList(@PathVariable String wordId, @RequestBody Posting posting);
 }

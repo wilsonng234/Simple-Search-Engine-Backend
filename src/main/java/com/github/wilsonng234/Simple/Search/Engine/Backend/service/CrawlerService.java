6c7ddf3b7fc2316d7a82588b1a45bbf454c524bf
Wilson Ng <wilsonng234@gmail.com>
2023-04-14 21:33:58 +0800

    Create CrawlerService

---
+++ src/main/java/com/github/wilsonng234/Simple/Search/Engine/Backend/service/CrawlerService.java
@@ -0,0 +1,13 @@
+package com.github.wilsonng234.Simple.Search.Engine.Backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrawlerService {
+    public boolean crawl(String url, String pages) {
+        // Return: a boolean value indicating whether the crawling was successful or not
+        // TODO: Implement this method
+
+        return false;
+    }
+}

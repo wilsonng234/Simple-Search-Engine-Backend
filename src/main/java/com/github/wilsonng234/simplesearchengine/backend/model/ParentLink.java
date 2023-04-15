9832882a3290e39b4ca04e0bb57e469b83443bbc
Wilson Ng <wilsonng234@gmail.com>
2023-04-15 16:22:32 +0800

    Remove docId field in parentLinks

--- src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/ParentLink.java
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/ParentLink.java
@@ -1,25 +1,19 @@
 package com.github.wilsonng234.simplesearchengine.backend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 @Document(collection = "parentLinks")
 public class ParentLink {
     @Id
-    private String docId;
-
-    @Indexed(unique = true)
     private String url;
 
     private List<String> parentUrls;
 
-    public ParentLink(String url, List<String> parentUrls) {
-        this.url = url;
-        this.parentUrls = parentUrls;
-    }
 }

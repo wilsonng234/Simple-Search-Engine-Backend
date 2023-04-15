c9f006c243b43a58b4efa12ce43c604fb57e0659
Wilson Ng <wilsonng234@gmail.com>
2023-04-15 15:53:10 +0800

    Create ParentLinkController

---
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/controller/ParentLinkController.java
@@ -0,0 +1,12 @@
+package com.github.wilsonng234.simplesearchengine.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/v1/parentLinks")
+public class ParentLinkController {
+
+}

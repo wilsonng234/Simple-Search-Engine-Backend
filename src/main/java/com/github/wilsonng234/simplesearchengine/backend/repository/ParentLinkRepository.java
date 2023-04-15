0bffed248b169e60c718213868095a3aed5911cc
Wilson Ng <wilsonng234@gmail.com>
2023-04-15 16:22:41 +0800

    Create findParentLinkByUrl method

--- src/main/java/com/github/wilsonng234/simplesearchengine/backend/repository/ParentLinkRepository.java
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/repository/ParentLinkRepository.java
@@ -4,6 +4,9 @@ import com.github.wilsonng234.simplesearchengine.backend.model.ParentLink;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParentLinkRepository extends MongoRepository<ParentLink, String> {
+    Optional<ParentLink> findParentLinkByUrl(String url);
 }

e259e32026b61e2fbf9a7431de863f408dee11c2
Wilson Ng <wilsonng234@gmail.com>
2023-04-15 16:06:22 +0800

    Create post method for parentLinks

--- src/main/java/com/github/wilsonng234/simplesearchengine/backend/controller/ParentLinkController.java
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/controller/ParentLinkController.java
@@ -1,12 +1,21 @@
 package com.github.wilsonng234.simplesearchengine.backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.wilsonng234.simplesearchengine.backend.model.ParentLink;
+import com.github.wilsonng234.simplesearchengine.backend.service.ParentLinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("api/v1/parentLinks")
 public class ParentLinkController {
+    @Autowired
+    private ParentLinkService parentLinkService;
 
+    @PostMapping
+    public ResponseEntity<ParentLink> createParentLink(@RequestBody ParentLink parentLink) {
+        return new ResponseEntity<>(parentLinkService.createParentLink(parentLink), HttpStatus.OK);
+    }
 }
--- src/main/java/com/github/wilsonng234/simplesearchengine/backend/service/ParentLinkService.java
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/service/ParentLinkService.java
@@ -1,13 +1,21 @@
 package com.github.wilsonng234.simplesearchengine.backend.service;
 
+import com.github.wilsonng234.simplesearchengine.backend.model.ParentLink;
 import com.github.wilsonng234.simplesearchengine.backend.repository.ParentLinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 public class ParentLinkService {
     @Autowired
     ParentLinkRepository parentLinkRepository;
 
-    
+    public ParentLink createParentLink(ParentLink parentLink) {
+        List<String> parentLinks = parentLink.getParentUrls() == null ? new LinkedList<>() : parentLink.getParentUrls();
+
+        return parentLinkRepository.insert(new ParentLink(parentLink.getUrl(), parentLinks));
+    }
 }

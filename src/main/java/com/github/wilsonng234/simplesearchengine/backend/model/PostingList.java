9a0c1dcbde15ffcee73d9ff2190e4e54bbc0aea8
Wilson Ng <wilsonng234@gmail.com>
2023-04-29 05:37:57 +0800

    Add `@CompoundIndex` for {wordId, type}

--- src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/PostingList.java
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/PostingList.java
@@ -3,15 +3,15 @@ package com.github.wilsonng234.simplesearchengine.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@CompoundIndex(def = "{'wordId': 1, 'type': 1}", unique = true)
 @Document(collection = "postingLists")
 public class PostingList {
-    @Id
     private String wordId;
     private String type;            // type.equals("title") || type.equals("body");
     private int maxTF = 0;

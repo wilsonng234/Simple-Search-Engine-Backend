703cfde1c2c5c5a5bb5453e4b15b74c59ed552c9
Wilson Ng <wilsonng234@gmail.com>
2023-05-01 03:00:51 +0800

    Create `NLPUtilsTest`

---
+++ src/test/java/com/github/wilsonng234/simplesearchengine/backend/util/NLPUtilsTest.java
@@ -0,0 +1,63 @@
+package com.github.wilsonng234.simplesearchengine.backend.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.util.Pair;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NLPUtilsTest {
+
+    @Test
+    void partsOfSpeech() {
+    }
+
+    @Test
+    void tokenize() {
+    }
+
+    @Test
+    void removeStopWords() {
+    }
+
+    @Test
+    void removeStopWordPosPairs() {
+    }
+
+    @Test
+    void removePunctuations() {
+    }
+
+    @Test
+    void removePunctuationsWordPosPairs() {
+    }
+
+    @Test
+    void stemWord() {
+    }
+
+    @Test
+    void stemWords() {
+    }
+
+    @Test
+    void stemWordPosPairs() {
+    }
+
+    @Test
+    void nGrams() {
+    }
+
+    @Test
+    void biGramWordPosPairs() {
+    }
+
+    @Test
+    void triGramWordPosPairs() {
+    }
+
+    @Test
+    void parsePhraseSearchQuery() {
+    }
+}

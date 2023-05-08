56f99f34b0312d20c96f14fb7d4776a8bce531c3
Wilson Ng <wilsonng234@gmail.com>
2023-05-08 22:31:50 +0800

    Change the Id of PageRank

--- src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/PageRank.java
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/PageRank.java
@@ -3,7 +3,7 @@ package com.github.wilsonng234.simplesearchengine.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @Document(collection = "pageRanks")
 public class PageRank {
-    @Id
+    @Indexed
     private String docId;
     private double pageRank;
 

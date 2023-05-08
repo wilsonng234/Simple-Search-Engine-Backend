8252669bc677f9bf62f4f47bc1470c433114390f
Wilson Ng <wilsonng234@gmail.com>
2023-05-09 04:45:32 +0800

    Add @Id field

--- src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/TermWeight.java
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/TermWeight.java
@@ -3,7 +3,7 @@ package com.github.wilsonng234.simplesearchengine.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Map;
@@ -13,7 +13,7 @@ import java.util.Map;
 @AllArgsConstructor
 @Document(collection = "termWeights")
 public class TermWeight {
-    @Indexed(unique = true)
+    @Id
     private String docId;
     private Map<String, Double> termWeights;     // non-zero term weights   (key: wordId, value: term weight)
 }

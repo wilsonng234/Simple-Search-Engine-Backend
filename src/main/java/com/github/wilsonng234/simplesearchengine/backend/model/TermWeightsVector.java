ef5b35a873a41836283596fa6b22e42adacee27b
Wilson Ng <wilsonng234@gmail.com>
2023-05-08 23:25:40 +0800

    Create termWeightsVectors collection

---
+++ src/main/java/com/github/wilsonng234/simplesearchengine/backend/model/TermWeightsVector.java
@@ -0,0 +1,19 @@
+package com.github.wilsonng234.simplesearchengine.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "termWeightsVectors")
+public class TermWeightsVector {
+    @Indexed
+    private String docId;
+    private Map<String, Double> termWeights;     // non-zero term weights   (key: wordId, value: term weight)
+}
